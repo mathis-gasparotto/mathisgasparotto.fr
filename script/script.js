dc8071c50554c3b0e43e33699fec57e1d2c3fa1d
Mathis-Gasparotto <mathis.gasparotto@hotmail.com>
2021-03-21 14:30:23 +0100

    Tout le code source de mon site perso (qui est en rénovation ;) )

--- script/script.js
+++ script/script.js
@@ -1,20 +1,47 @@
 $(document).ready(function() {
 
-    const navbar = $("#main-menu");
-    const button = $("#top_button");
+  const navbar = $("#main-menu");
+  const button = $("#top_button");
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() <= 80) {
-            navbar.removeClass("nav-scroll");
+  $(window).scroll(function() {
+    if ($(window).scrollTop() <= 80) {
+      navbar.removeClass("nav-scroll");
 
-            button.removeClass("top_button");
-            button.addClass("top_button_del");
-        } else {
-            navbar.addClass("nav-scroll");
+      button.removeClass("top_button");
+      button.addClass("top_button_del");
+    } else {
+      navbar.addClass("nav-scroll");
 
-            button.addClass("top_button");
-            button.removeClass("top_button_del");
-        }
-    });
+      button.addClass("top_button");
+      button.removeClass("top_button_del");
+    }
+  });
 
 })
+
+$(document).ready(function() {
+  $("#qui_button").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top
+    }, 500);
+  });
+});
+
+$(document).ready(function() {
+  $("#top_button a").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top
+    }, 500);
+  });
+});
+
+$(document).ready(function() {
+  $("#top_logo").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: $($(this).attr("href")).offset().top
+    }, 500);
+  });
+});
