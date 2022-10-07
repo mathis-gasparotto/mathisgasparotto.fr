71a6161de68822aa1bd73eb53a0c3033f13d2b86
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2022-10-07 07:57:33 +0200

    :fire: Clean code
    Remove all try for the main page animation

--- data/script/main.js
+++ data/script/main.js
@@ -41,18 +41,8 @@ $(document).ready(function() {
   
   const hamburger_menu = document.querySelector(".hamburger-menu");
 
-  // -- Test for animation to return to the page from the nav menu --
-  // const hamburger_menu_active = document.querySelector(".screen-container.active .hamburger-menu");
-  // ----------------
-
   const container = document.querySelector(".screen-container");
-
-  // -- Test for animation to return to the page from the nav menu --
-  // const page_content = document.querySelector(".page-content");
-  // ----------------
   
- 
- 
   const click_to_select = document.querySelector(".screen-container .main .click-to-select");
  
   const home_page = document.querySelector(".screen-container .main-container .home-page");
@@ -128,22 +118,7 @@ $(document).ready(function() {
     container.classList.toggle("active");
     
     const nav_links = document.querySelectorAll(".nav-link");
-
-    // -- Test for animation to return to the page from the nav menu --
-    // page_content.classList.add('active');
-    
-
-    // main_active.addEventListener("click", () => {
-    //   container.classList.remove("active");
-    // });
-    
-    // home_btn.addEventListener("click", () => {
-    //   container.classList.remove("active");
-    // });
-
-    // ----------------
        
-    
     click_to_select.addEventListener("click", () => {
       container.classList.remove("active");
     });
@@ -156,24 +131,4 @@ $(document).ready(function() {
     
   });
   
-  // -- Test for animation to return to the page from the nav menu --
-  // hamburger_menu_active.addEventListener("click", () => {
-  //   //wait
-  //   page_content.classList.remove('active');
-  // });
-  // ----------------
-
-  /* -- Disparition du header en scroll vers le bas, et apparition en scroll vers le haut --
-  
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.querySelector(".screen-container .navbar").style.top = "0";
-    } else {
-      document.querySelector(".screen-container .navbar").style.top = "-100px";
-    }
-    prevScrollpos = currentScrollPos;
-  }*/
-
 })
