9b1fea8ac276517d428363b3d9b18ccff6fbb17a
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2022-12-07 17:21:13 +0100

    :wastebasket: remove phone num

--- components/coordinates.php
+++ components/coordinates.php
@@ -7,15 +7,6 @@ $linkedin_url = 'https://www.linkedin.com/in/mathis-gasparotto/';
 <section class="section coordinates bg-light">
   <address class="container">
     <div class="single-coordinate">
-      <a href="<?php echo $tel_url; ?>">
-        <i class="fa fa-phone icon"></i>
-      </a>
-      <h3 class="title">Téléphone</h3>
-      <a href="<?php echo $tel_url; ?>">
-        <h4 class="text">06 03 33 67 32</h4>
-      </a>
-    </div>
-    <div class="single-coordinate">
       <a href="<?php echo $email_url; ?>">
         <i class="fa fa-envelope icon"></i>
       </a>
