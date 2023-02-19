9925a8e40aa0d436f2ac4c5256e171bfd3d1827d
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-02-19 03:33:28 +0100

    :bug: try to fix contact form bug

--- components/contact_form.php
+++ components/contact_form.php
@@ -64,7 +64,44 @@ if (!empty($_POST)) {
       <h2 class="title">Envoyez-moi un message</h2>
     </div>
     <div class="form-container">
-      <form method="POST" class="form" action="/contact/">
+      <form method="POST" class="form desktop" action="/contact/">
+        <?php if (isset($sendSuccess) && $sendSuccess) { ?>
+          <div class="alert alert-success send-info"><?php echo $sendSuccess; ?></div>
+        <?php } ?>
+        <?php if (isset($sendError) && $sendError) { ?>
+          <div class="alert alert-danger send-infos"><?php echo $sendError; ?></div>
+        <?php } ?>
+        <div class="input-container">
+          <label for="lname" class="required">Nom</label>
+          <input class="<?php echo((!empty($errors['lname'])) ? 'is-invalid' : '') ?>" type="text" id="lname" name="lname" placeholder="Votre nom" />
+          <?php if(isset($errors['lname'])){ ?>
+            <div class="invalid-feedback"><?php echo $errors['lname']; ?></div>
+          <?php } ?>
+        </div>
+        <div class="input-container">
+          <label for="fname" class="required">Prénom</label>
+          <input class="<?php echo((!empty($errors['fname'])) ? 'is-invalid' : '') ?>" type="text" id="fname" name="fname" placeholder="Votre prénom" />
+          <?php if(isset($errors['fname'])){ ?>
+            <div class="invalid-feedback"><?php echo $errors['fname']; ?></div>
+          <?php } ?>
+        </div>
+        <div class="input-container">
+          <label for="email" class="required">Email</label>
+          <input class="<?php echo((!empty($errors['email'])) ? 'is-invalid' : '') ?>" type="text" id="email" name="email" placeholder="Adresse Email" />
+          <?php if(isset($errors['email'])){ ?>
+            <div class="invalid-feedback"><?php echo $errors['email']; ?></div>
+          <?php } ?>
+        </div>
+        <div class="input-container">
+          <label for="message" class="required">Message</label>
+          <textarea class="<?php echo((!empty($errors['message'])) ? 'is-invalid' : '') ?>" name="message" id="message" cols="30" rows="10" placeholder="Indiquez votre message ici"></textarea>
+          <?php if(isset($errors['message'])){ ?>
+            <div class="invalid-feedback"><?php echo $errors['message']; ?></div>
+          <?php } ?>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Envoyer" />
+      </form>
+      <form method="POST" class="form mobile" action="/#contact-form">
         <?php if (isset($sendSuccess) && $sendSuccess) { ?>
           <div class="alert alert-success send-info"><?php echo $sendSuccess; ?></div>
         <?php } ?>
