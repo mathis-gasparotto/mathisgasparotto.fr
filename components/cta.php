0ff6455f4cc2529b8cfbf2408c1a55519a9beaf1
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-05 12:36:42 +0200

    wording: "vos prochains projets" on cta

--- components/cta.php
+++ components/cta.php
@@ -1,6 +1,6 @@
 <section class="section cta bg-secondary">
   <div class="container">
-    <h2 class="title cl-white">Travaillons ensemble sur votre prochain projet</h2>
+    <h2 class="title cl-white">Travaillons ensemble sur vos prochains projets</h2>
     <a href="<?php echo $contact_url; ?>" class="btn btn-primary changing-page-link contact_link scroll-to-top">Contactez-moi</a>
   </div>
 </section>
