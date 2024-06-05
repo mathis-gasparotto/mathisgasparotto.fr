088f194035bf6c9ecfcc17f11bb297c82d59a193
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-05 19:54:06 +0200

    wording: who component

--- components/who.php
+++ components/who.php
@@ -6,7 +6,7 @@
     <div class="right content-container">
       <h2 class="title">Qui suis-je ?</h2>
       <p class="text">
-        Je suis un jeune étudiant passionné par l'informatique en général, mais surtout par le développement d'application en tout genre.<br/> J'étudie actuellement à l'école de MyDigitalSchool en 1ère année de Master Développeur Full-Stack, suite à mon diplôme de Concepteur Développeur d'Application au sein de cette même école. C'est de plus dans cette école que j'ai découvert une vraie passion pour le <strong>développement web</strong>. Je suis surtout passionné par les projets avec du <strong>développement Back-End</strong> et du <strong>développement Front-End</strong> de mélangés, avec un petit penchant pour <strong>VueJS</strong>, <strong>API Platform</strong> et <strong>Symfony</strong>.
+        Je suis un jeune étudiant passionné par le développement informatique en général, mais surtout par le développement d'application en tout genre.<br/> J'étudie actuellement à l'école de MyDigitalSchool en 1ère année de Master Développeur Full-Stack, suite à mon diplôme de Concepteur Développeur d'Application au sein de cette même école. C'est de plus dans cette école que j'ai découvert une vraie passion pour le <strong>développement web</strong>. Je suis surtout passionné par les projets avec du <strong>développement Back-End</strong> et du <strong>développement Front-End</strong> de mélangés, avec un petit penchant pour <strong>VueJS</strong>, <strong>API Platform</strong> et <strong>Symfony</strong>.
       </p>
       <a href="<?php echo $cv_url; ?>" class="btn btn-primary changing-page-link cv_link scroll-to-top">Mon CV</a>
     </div>
