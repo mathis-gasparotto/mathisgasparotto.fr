0e536e173f1c470209e78678c4d8296f7e4bdbc2
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-05 11:18:26 +0200

    feat: update futur project section

--- components/futur_projects.php
+++ components/futur_projects.php
@@ -2,20 +2,20 @@
   <div class="container">
     <h2 class="title">Projet en cours</h2>
     <div class="projects-container solo">
-      <!-- <a class="first project-container" href="#"> -->
+      <?php /* <a class="first project-container" href="#"> */ ?>
       <div class="first project-container">
         <div class="project-img-container">
-          <img src="/data/img/application-mobile-paris-entre-amis-mathis-gasparotto-web-developpeur-react-native-projet-en-cours.jpg" alt="application-mobile-paris-entre-amis-mathis-gasparotto-web-developpeur-react-native-projet-en-cours" title="Illustration du projet en cours d'application mobile de paris entre amis" class="project-img img" loading="lazy" />
-          <h3 class="project-title">Application mobile</h3>
+          <img src="/data/img/application-avec-ia-mathis-gasparotto-web-developpeur-quasar-api-platform-symfony-projet-en-cours.jpg" alt="application-avec-ia-mathis-gasparotto-web-developpeur-quasar-api-platform-symfony-projet-en-cours" title="Illustration du projet en cours d'application avec consommation d'IA sous Quasar, API Platform et Symfony" class="project-img img" loading="lazy" />
+          <h3 class="project-title">Application avec IA</h3>
         </div>
       </div>
-      <!-- <a class="second project-container" href="#"> -->
-      <!-- <div class="second project-container">
+      <?php /* <a class="second project-container" href="#"> */ ?>
+      <?php /* <div class="second project-container">
         <div class="project-img-container">
           <img src="/data/img/008899_portrait.png" alt="Image de projet" title="Image de projet" class="project-img img" loading="lazy" />
           <h3 class="project-title">Titre du projet</h3>
         </div>
-      </div> -->
+      </div> */ ?>
     </div>
   </div>
 </section>
